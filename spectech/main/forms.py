ddf31740c4dee12653f67ce65921b6d4d5ad5556
KennyMc11 <maksim6316@mail.ru>
2026-03-19 14:59:44 +0000

    Отдельные вход и регистрация, НЕГОТОВЫ

---
+++ spectech/main/forms.py
@@ -0,0 +1,12 @@
+# file: myapp/forms.py
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# Расширяем стандартную форму регистрации Django
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()  # Добавляем поле email
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- spectech/main/views.py
+++ spectech/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -8,3 +10,9 @@ def index(request):
 
 def about(request):
     return HttpResponse('About page')
+
+def register(request):
+    return render(request, 'main/register.html')
+
+def login(request):
+    return render(request, 'main/login.html')
